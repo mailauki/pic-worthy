e65a35a2bbe36bc897d297fbcd232d74cee49ab2
Julie Evans <juliemevans17@gmail.com>
2022-07-31 14:17:02 -0500

    Added totaling methods to  models

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,11 @@
 class Photo < ApplicationRecord
   belongs_to :user
 
+  has_many :likes
   has_many :photo_tags
   has_many :tags, through: :post_tags
+
+  def likes_total
+    self.likes.length
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,16 @@ class User < ApplicationRecord
   has_many :followees, through: :followed_users
   has_many :following_users, foreign_key: :followee_id, class_name: "Friendship"
   has_many :followers, through: :following_users
+
+  def photos_total
+    self.photos.length
+  end
+
+  def followers_total
+    self.followers.length
+  end
+
+  def followees_total
+    self.followees.length
+  end
 end
