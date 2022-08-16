ff237ad1ac7cebce6ece6f947515221464d2ec5b
Julie Evans <juliemevans17@gmail.com>
2022-08-16 15:52:31 -0500

    Added showPassword button

--- client/src/components/FormInput.js
+++ client/src/components/FormInput.js
@@ -1,6 +1,15 @@
-import { TextField } from '@mui/material';
+import { useState } from 'react';
+import { TextField, InputAdornment, IconButton } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 function FormInput({ errors, item, formData, setFormData }) {
+  const [showPassword, setShowPassword] = useState(false)
+
+  function handleShowPassword() {
+    setShowPassword(!showPassword)
+  }
+
   return (
     errors && errors.length > 0 ? (
       <TextField
@@ -17,11 +26,26 @@ function FormInput({ errors, item, formData, setFormData }) {
     ) : (
       <TextField
         label={item.label}
-        type={item.type}
+        type={!showPassword ? item.type : "text"}
         margin="normal"
         value={item.value}
         onChange={(event) => setFormData({...formData, [item.name]: event.target.value})}
         helperText={item.helper}
+        InputProps={{
+          endAdornment: item.type === "password" ? (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleShowPassword}
+                  edge="end"
+                >
+                  {!showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ) : (
+              <></>
+            )
+        }}
         className="form-input"
         key={item.name}
       />
